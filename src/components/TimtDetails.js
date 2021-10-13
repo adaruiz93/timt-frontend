da9fd933391645b8d29e383adc631915eef4e521
adaruiz <ada.ruiz93@gmail.com>
2021-10-13 15:32:51 -0700

    finished details page

--- src/components/TimtDetails.js
+++ src/components/TimtDetails.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom'
 
 const TimtDetails = (props) => {
-  return (
-    <p>Details -_-</p>
+  const currentId = props.match.params.id
+  const [timts, setTimts] = useState([])
+  const [loading, setLoading] = useState(true)
 
+  const getTimts = async (id) => {
+    const foundTimt = await fetch(`http://localhost:9000/timt/${id}`)
+    const parsed = await foundTimt.json()
+    setTimts(parsed)
+    setLoading(false)
+  }
+  useEffect(() => {
+    getTimts(currentId)
+  }, [])
+
+  return (
+    <>
+    {
+      loading ? <h3>Loading...</h3> :
+        <div>
+          <h1>{timts.mood}</h1>
+          {timts.comment}
+          <Link to='/timt'>Back</Link>
+          <Link to={`/timt/:id/edit`}>Edit</Link>
+        </div>
+    }
+    </>
   )
 }
 
